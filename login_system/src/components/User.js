823a16014d6e3a884c992c7a4166c79834487c13
kireeti <rayaprolu.kireeti@gmail.com>
2022-05-21 20:29:21 -0400

    First Functional Component

---
+++ login_system/src/components/User.js
@@ -0,0 +1,11 @@
+import React from "react";
+//functional react component
+function User(){
+    return(
+        <div className="container">
+            <h2>User Functional Component</h2>
+        </div>
+
+    )
+} 
+export default User;
